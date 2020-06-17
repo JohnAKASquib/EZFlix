97cfada72465c242d43da91bbd2a93023fbf4fd2
John Stendardo <johnakasquib96@gmail.com>
2020-06-17 18:21:32 -0400

    adding search features to new design

--- src/components/containers/NavBarContainer.js
+++ src/components/containers/NavBarContainer.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import NavBarView from "../views/NavBarView";
-import SearchBarView from "../views/SearchBarView";
-
+import SearchBarContainer from "../containers/SearchBarContainer";
 
 class NavBarContainer extends Component {
   render() {
     return (
-    <>
-    <NavBarView />
-    <SearchBarView/>
-    </>
+      <>
+        <NavBarView />
+        <SearchBarContainer />
+      </>
     );
   }
 }
