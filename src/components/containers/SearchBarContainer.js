93fe9ebb6a9c232bbc2d1a9f26ce4de2b130523e
John Stendardo <johnakasquib96@gmail.com>
2020-06-16 21:45:11 -0400

    added a searchbar with a bit of styling

---
+++ src/components/containers/SearchBarContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SearchBarView from "../views/SearchBarView";
+
+class SearchBarContainer extends Component {
+  render() {
+    return <SearchBarView />;
+  }
+}
+
+export default SearchBarContainer;
