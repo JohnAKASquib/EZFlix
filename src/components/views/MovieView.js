f9043a8b531486ee636ed7b46f677ebc65f2eae5
CyberAlv <munaralvin@gmail.com>
2020-06-18 21:32:46 -0400

    made some design changes to home and movie page

--- src/components/views/MovieView.js
+++ src/components/views/MovieView.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./styles/MovieView.css";
 
+
 const MovieView = (props) => {
     return (
-      <div className="movie-page">
-        <div className="container1">
+      <div className="container1">
           <div
-            className="card text-center mt-5 mb-5"
+            className="card1 text-center mt-5 mb-5"
             key={props.movie.id}
             style={{ width: 60 + "rem" }}
            >
@@ -26,7 +26,6 @@ const MovieView = (props) => {
                       <h3>Voter's Rating</h3>
                       <p>{props.movie.vote_average}</p>
                       <p>{props.movie.overview}</p>
-        
             <div className="card-footer1 text-muted">           
                         <h2>Release Date</h2>
                   <p>{props.movie.release_date ? props.movie.release_date : props.movie.first_air_date}</p>
@@ -34,7 +33,7 @@ const MovieView = (props) => {
           </div>
         </div>
       </div>
-      </div>
+          
     );
 }
 
